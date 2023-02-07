dddc9ef7bdd7af60a3a656447334482bfb9838c6
Anton Charapanau <charapanauanton@gmail.com>
2023-02-07 23:50:23 +0000

    01

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import {TaskType, TodoList} from "./TodoList";
+
+
+function App() {
+
+    let tasks1: Array<TaskType> = [
+        {id:1, title: "CSS", isDone: true },
+        {id:2, title: "JS", isDone: true },
+        {id:3, title: "React", isDone: false }
+    ]
+    let tasks2: Array<TaskType> = [
+        {id:1, title: "Terminator", isDone: true },
+        {id:2, title: "XXX", isDone: false },
+        {id:3, title: "Taxi", isDone: true }
+    ]
+
+    return (
+        <div className="App">
+            <TodoList title="What to learn" tasks={tasks1}/>
+            <TodoList title="Movies" tasks={tasks2}/>
+        </div>
+    );
+}
+
+
+export default App;
